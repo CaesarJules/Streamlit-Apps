7e25586345253d68d3633c68e77ceda894313a8c
CaesarJules <42262367+CaesarJules@users.noreply.github.com>
2022-01-13 23:36:11 -0500

    COVID19 Dashboard - beta version

---
+++ covid19stats-app.py
@@ -0,0 +1,284 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+from plotly.offline import iplot
+import plotly.graph_objs as go
+import plotly.express as px
+import json 
+import datetime
+from collections import defaultdict
+import pickle5
+import pickle
+import os
+import warnings
+from IPython.display import Markdown as md
+from pytz import timezone
+from functions import *
+
+st.set_page_config(layout='wide')
+
+warnings.filterwarnings('ignore')
+#Get the current file's directory
+path = os.path.dirname(__file__)
+#Create cache's filepath
+filepath = os.path.join(path, 'cache_final.p')
+
+fig = go.Figure()
+fig2 = go.Figure()
+fig2_title=f""" """
+fig3 = go.Figure()
+fig3_title=f""" """
+
+#st.markdown("<h1 style='text-align: justified;'>Covid19 Dashboard</h1>", unsafe_allow_html=True)
+st.title("COVID19 Dashboard")
+st.markdown("***")
+#Initialize the starting date in our data
+initial_date = datetime.datetime.today()
+dates = get_dates_till_today(initial_date)
+
+@st.cache(show_spinner=False)
+def get_regions_list():
+    url = "https://covid-19-statistics.p.rapidapi.com"
+    headers = {
+        'x-rapidapi-host': 'covid-19-statistics.p.rapidapi.com',
+        'x-rapidapi-key': '44e824ba26mshbd88513db1a43d3p174f66jsn2f60602ce307'
+    }
+    #Get countries names and iso
+    response = requests.request(method="GET", url=f"{url}/regions", headers=headers)
+    #json.loads(response.text)['data']
+    response_json = json.loads(response.text)['data']
+
+    return pd.json_normalize(response_json)
+
+df_regions = get_regions_list()
+df_regions = df_regions.sort_values('name').reset_index(drop=True)
+regions = list(df_regions.name)
+#Clean the list of regions
+regions = [x for x in regions if x not in ['Others','Cruise Ship']]
+iso = [df_regions.loc[df_regions.name==rgn, 'iso'].values[0] for rgn in regions]
+
+@st.cache(show_spinner=False)
+def load_cache(filepath=filepath):
+    #Check first if there is a cached file and if it can be accessed
+    if os.path.isfile(filepath) and os.access(filepath, os.R_OK):
+        with open(filepath, 'rb') as fp:
+            return pickle5.load(fp)
+    else:
+        return {}
+
+cached_data = load_cache()
+
+#Use the first date in the data to cache all data
+latest_cache_date = initial_date
+data = dict()
+
+if len(cached_data)>0:
+    #Get the latest cache date
+    data = cached_data.copy()
+    temp = data['dates'][-1]
+    latest_cache_date = datetime.datetime.strptime(temp, '%Y-%m-%d')
+    #Update the initial date to the first date in our cached data
+    initial_date = data['dates'][0]
+    initial_date = datetime.datetime.strptime(initial_date, '%Y-%m-%d')
+    dates = get_dates_till_today(initial_date)
+    #Update cached data  
+    cache_data(data, latest_cache_date, regions, df_regions)
+else:
+    print("Error, No cache found!!!")
+
+
+#Create and populate the sidebar
+latest_ww = get_latest_worldwide_data()
+latest_update = datetime.datetime.strptime(latest_ww['last_update'], 
+'%Y-%m-%d %H:%M:%S').strftime('%B %d, %Y at %I:%M %p')
+st.sidebar.markdown(f"""<p style="font-size:22px;">
+                        Total Confirmed Cases:<br>
+                        <span style="color: #FF0000 ;text-align: center ;font-weight:bold; ">{latest_ww["confirmed"]:,}</span><br>
+                        Total Deaths :<br>
+                        <span style="color: #B8860B ; text-align: center ;font-weight:bold; ">{latest_ww["deaths"]:,}</span> <br>
+                        <span style="font-size:20px; color: #778899 ">
+                        Last Updated on: <br>
+                        {latest_update}</span>
+                        </p>
+                        """, unsafe_allow_html=True
+                    )
+
+st.sidebar.markdown("***")
+
+st.sidebar.header('Filter your search')
+graph_type = st.sidebar.selectbox('Cases type',('confirmed','deaths','new_cases'))
+st.sidebar.subheader('Search by country or region 📍')
+countries_selected = st.sidebar.multiselect('Country', regions)
+if graph_type!='new_cases':
+    num_weeks_selected = st.sidebar.slider('Number of weeks to display', 1, 5, 3)
+
+#Plot the world heat map
+#Use the selected graph_type
+
+st.markdown(f"""<h4 style='text-align: justified;'>Number of coronavirus (COVID-19) cases worldwide on {latest_cache_date.strftime('%A %B %d, %Y')}</h4>""", unsafe_allow_html=True)
+
+col1, col2, col3 = st.columns(3)
+col1.metric("Confirmed cases", f"{data['global_conf_diff'][-1]}", 
+f"{round((data['global_conf_diff'][-1] - data['global_conf_diff'][-2])/data['global_conf_diff'][-2], 2)}")
+
+col2.metric("Deaths", f"{data['global_deaths_diff'][-1]}", 
+f"{round((data['global_deaths_diff'][-1] - data['global_deaths_diff'][-2])/data['global_deaths_diff'][-2], 2)}")
+
+col3.metric("New cases", f"{int(data['global_new_cases'][-1])}", 
+f"{round((data['global_new_cases'][-1] - data['global_new_cases'][-2])/data['global_new_cases'][-2], 2)}")
+st.markdown("***")
+
+map_subtitle = "COVID-19 new cases worldwide"
+if graph_type!='new_cases':
+    map_subtitle = f"COVID-19 {graph_type} cases for the last {num_weeks_selected*7} days"
+
+fig.update_layout(
+    geo=dict(
+        showframe=False,
+        showcoastlines=False,
+        projection_type='equirectangular'
+    ),
+    overwrite = True,
+    width=1200,
+    height=800,
+    annotations = [dict(
+        x=0.55,
+        y=0.15,
+        xref='paper',
+        yref='paper',
+        text= map_subtitle,
+        showarrow = False,
+        font_color = 'black',
+        font_size = 30
+    )]
+)
+fig2.update_layout(
+    overwrite = True,
+    width=1100,
+    height=600
+)
+fig3.update_layout(
+    overwrite = True,
+    width=1100,
+    height=600
+)
+
+if graph_type=='new_cases':
+    if len(countries_selected)>0:
+
+        fig2_title = f"""<h4 style='text-align: justified;'>Number of COVID-19 new cases  in {", ".join(countries_selected)} as of {latest_cache_date.strftime('%A %B %d, %Y')}</h4>"""
+
+        fig2.add_trace(go.Bar(
+            x=[data['new_cases_per_rgn'][x] for x in countries_selected],
+            y=countries_selected,
+            orientation='h',
+            text=[int(data['new_cases_per_rgn'][x]) for x in countries_selected],
+            textposition='auto'
+            )
+        )
+
+
+    else:
+        fig.add_trace(go.Choropleth(
+        locations = iso,
+        z = list(data['new_cases_per_rgn'].values()),
+        text = regions,
+        colorscale = 'Reds',
+        autocolorscale=False,
+        marker_line_color='darkgray',
+        marker_line_width=0.5,
+        colorbar_title = "Number of cases",
+        ))
+
+        fig2.add_trace(go.Bar(
+            x=[y for x,y in data['top5_rgn_new_cases']],
+            y=[x for x,y in data['top5_rgn_new_cases']],
+            orientation='h',
+            text=[int(y) for x,y in data['top5_rgn_new_cases']],
+            textposition='auto'
+            )
+        )
+        fig2_title = f"""<h4 style='text-align: justified;'>Top 5 countries with the highest number of COVID-19 new cases as of {latest_cache_date.strftime('%A %B %d, %Y')}</h4>"""
+
+else:
+    global_case_keys = {'rgn':{
+                                'confirmed':'conf_per_rgn',
+                                'deaths':'deaths_per_rgn'
+                            },
+                        'glbl':{
+                                'confirmed':'global_conf_diff',
+                                'deaths':'global_deaths_diff'
+                            },
+                        'ctr':{
+                                'confirmed':'confirmed_diff',
+                                'deaths':'deaths_diff'
+                            }
+                        }
+
+    if len(countries_selected)>0:
+
+        fig2_title = f"""<h4 style='text-align: justified;'>Daily number of COVID-19 {graph_type} cases in {", ".join(countries_selected)} for the last {num_weeks_selected*7} days</h4>"""
+
+        fig3_title = f"""<h4 style='text-align: justified;'>Daily number of COVID-19 {graph_type} cases in {", ".join(countries_selected)} since {datetime.datetime.strptime(data['dates'][0], "%Y-%m-%d").strftime('%A %B %d, %Y')}</h4>"""
+
+        for x in countries_selected:
+            fig2.add_trace(go.Scatter(
+                x=data['dates'][-(num_weeks_selected*7):], 
+                y=data[x][global_case_keys['ctr'][graph_type]][-(num_weeks_selected*7):],
+                mode='lines',
+                name=x
+                )
+            )
+
+            fig3.add_trace(go.Bar(
+                x=data['dates'], 
+                y=data[x][global_case_keys['ctr'][graph_type]],
+                name=x
+                )
+            )
+
+
+    
+    else:
+        fig.add_trace(go.Choropleth(
+            locations = iso,
+            z = data[f"top{num_weeks_selected}_wk"][global_case_keys['rgn'][graph_type]],
+            text = regions,
+            colorscale = 'Reds',
+            autocolorscale=False,
+            marker_line_color='darkgray',
+            marker_line_width=0.5,
+            colorbar_title = "Number of cases",
+        ))
+
+        fig2.add_trace(go.Bar(
+            x=data['dates'][-(num_weeks_selected*7):],
+            y=data[global_case_keys['glbl'][graph_type]][-(num_weeks_selected*7):],
+            )
+        )
+        fig2_title = f"""<h4 style='text-align: justified;'>Daily number of COVID-19 {graph_type} cases worldwide over the last {num_weeks_selected*7} days</h4>"""
+
+        fig3.add_trace(go.Scatter(
+            x=data['dates'], 
+            y=data[global_case_keys['glbl'][graph_type]],
+            mode='lines+markers',
+            name='lines+markers'
+            )
+        )
+        fig3_title = f"""<h4 style='text-align: justified;'>Daily number of COVID-19 {graph_type} cases worldwide since {datetime.datetime.strptime(data['dates'][0], "%Y-%m-%d").strftime('%A %B %d, %Y')}</h4>"""
+
+if len(countries_selected)<=0:
+    st.plotly_chart(fig)
+    st.markdown(fig2_title, unsafe_allow_html=True)
+    st.plotly_chart(fig2)
+    if graph_type!='new_cases':
+        st.markdown(fig3_title, unsafe_allow_html=True)
+        st.plotly_chart(fig3)
+else:
+    st.markdown(fig2_title, unsafe_allow_html=True)
+    st.plotly_chart(fig2)
+    if graph_type!='new_cases':
+        st.markdown(fig3_title, unsafe_allow_html=True)
+        st.plotly_chart(fig3)
+
---
+++ functions.py
@@ -0,0 +1,150 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import requests
+from plotly.offline import iplot
+import plotly.graph_objs as go
+import plotly.express as px
+import json 
+from streamlit.script_runner import StopException, RerunException
+import datetime
+from collections import defaultdict
+import pickle5
+import pickle
+import os
+import warnings
+from pytz import timezone
+
+def get_dates_till_today(start_date):
+  return pd.date_range(end=datetime.datetime.today(), start=start_date).strftime('%Y-%m-%d').tolist()
+
+def get_region_report(region_name, date, df):
+  url = "https://covid-19-statistics.p.rapidapi.com"
+  headers = {
+      'x-rapidapi-host': 'covid-19-statistics.p.rapidapi.com',
+      'x-rapidapi-key': '44e824ba26mshbd88513db1a43d3p174f66jsn2f60602ce307'
+    }
+  #Get countries names and iso
+  response = requests.request(method="GET", url=f"{url}/reports", 
+              params={'iso': df.loc[df.name == region_name, 'iso'], 
+                'region_name': region_name, 'date': date}, headers=headers)
+  response_json = json.loads(response.text)
+
+  if len(response_json) > 0:
+    return response_json['data']
+
+  return []
+
+def get_worldwide_data(date):
+    url = "https://covid-19-statistics.p.rapidapi.com"
+    headers = {
+        'x-rapidapi-host': 'covid-19-statistics.p.rapidapi.com',
+        'x-rapidapi-key': '44e824ba26mshbd88513db1a43d3p174f66jsn2f60602ce307'
+    }
+    #Get countries names and iso
+    response = requests.request(method="GET", url=f"{url}/reports/total",params={'date':date}, headers=headers)
+    #json.loads(response.text)['data']
+    response_json = json.loads(response.text)
+
+    if len(response_json) > 0:
+      return response_json['data']
+
+    return []
+@st.cache(show_spinner=False)
+def get_latest_worldwide_data():
+    url = "https://covid-19-statistics.p.rapidapi.com"
+    headers = {
+        'x-rapidapi-host': 'covid-19-statistics.p.rapidapi.com',
+        'x-rapidapi-key': '44e824ba26mshbd88513db1a43d3p174f66jsn2f60602ce307'
+    }
+    #Get countries names and iso
+    response = requests.request(method="GET", url=f"{url}/reports/total", headers=headers)
+    #json.loads(response.text)['data']
+    response_json = json.loads(response.text)
+
+    if len(response_json) > 0:
+      return response_json['data']
+
+    return []
+
+def get_countries_data(dates, regions, df_regions):
+  temp = defaultdict(list)
+  result = defaultdict(dict)
+  for region in regions:
+    for date in dates:
+      sol = {'confirmed_diff':0, 'deaths_diff':0, 'date':date}
+      data = get_region_report(region, date, df_regions)
+      if len(data)> 0:
+        for report in data:
+          sol['confirmed_diff'] += report['confirmed_diff']
+          sol['deaths_diff'] += report['deaths_diff']
+      temp[region].append(sol)
+
+  for region in regions:
+    result[region]['confirmed_diff'] = np.array([sub['confirmed_diff'] for sub in temp[region]])
+    result[region]['deaths_diff'] = np.array([sub['deaths_diff'] for sub in temp[region]])
+  result['dates'] = np.array(dates)
+
+  #Get global data and append it to the result
+  glob_data = defaultdict(list)
+  for date in dates:
+    data = get_worldwide_data(date)
+    glob_data['global_conf_diff'].append(data['confirmed_diff'])
+    glob_data['global_deaths_diff'].append(data['deaths_diff'])
+
+  result['global_conf_diff'] = np.array(glob_data['global_conf_diff'])
+  result['global_deaths_diff'] = np.array(glob_data['global_deaths_diff'])
+
+  return result
+@st.cache(show_spinner=False)
+def get_latest_weekly_data(data, regions, n_weeks):
+  sol = {'conf_per_rgn': np.array([np.sum(data[rgn]['confirmed_diff'][-(7*n_weeks):]) for rgn in regions]), 'deaths_per_rgn': np.array([np.sum(data[rgn]['deaths_diff'][-(7*n_weeks):]) for rgn in regions])}
+  return sol
+
+def get_optimized_regions_data(data, regions):
+  result = data.copy()
+  #Get new cases globally and per region
+  #New cases equals to the average of confirmed cases over the last 7 days
+  result['global_new_cases'] = np.array([np.mean(result['global_conf_diff'][-8:-1]), 
+  np.mean(result['global_conf_diff'][-7:])])
+  result['new_cases_per_rgn'] = {k:np.mean(result[k]['confirmed_diff'][-7:]) for k in regions}
+  result['top5_rgn_new_cases'] = sorted(result['new_cases_per_rgn'].items(), key=lambda x: x[1], reverse=True)[:5]
+
+  #Get the data for the latest 1 to 5 weeks
+  for i in range(1,6):
+    result[f"top{i}_wk"] = get_latest_weekly_data(result, regions, i)
+  
+  return result.copy()
+
+def update_cached_data(data, new_data, regions):
+  old_data = data.copy()
+  for rgn in regions:
+    old_data[rgn]['confirmed_diff'] = np.append(old_data[rgn]['confirmed_diff'][:-1], 
+    new_data[rgn]['confirmed_diff'])
+    old_data[rgn]['deaths_diff'] = np.append(old_data[rgn]['deaths_diff'][:-1], 
+    new_data[rgn]['deaths_diff'])
+
+  old_data['dates'] = np.append(old_data['dates'][:-1], new_data['dates'])
+  old_data['global_conf_diff'] = np.append(old_data['global_conf_diff'][:-1], new_data['global_conf_diff'])
+  old_data['global_deaths_diff'] = np.append(old_data['global_deaths_diff'][:-1], 
+  new_data['global_deaths_diff'])
+
+  return old_data.copy()
+
+def cache_data(data, latest_date, regions, df_regions):
+    time1 = datetime.datetime.strptime(datetime.datetime.now(timezone('Canada/Eastern')).strftime('%Y-%m-%d'), '%Y-%m-%d')
+    if time1 > (latest_date + datetime.timedelta(days=10)):
+        new_dates = get_dates_till_today(latest_date)
+        #Remove today's date from the dates to be cached
+        new_dates.pop(-1)
+        if len(new_dates)>0:
+            new_data = get_countries_data(new_dates, regions, df_regions)
+            #Update the data
+            #Debugging !!!!
+            st.write("Caching the data!!!")
+
+            temp = update_cached_data(data, new_data, regions)
+            data = get_optimized_regions_data(temp, regions)
+            #Cache the new data
+            with open(filepath, 'wb') as fp:
+                pickle.dump(data, fp, protocol=pickle.HIGHEST_PROTOCOL)
